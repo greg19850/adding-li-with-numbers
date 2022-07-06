f465be9992d61e80fff763f7a00b7a7c3fb71ec9
greg@gregPredator <greg19850@gmail.com>
2022-07-06 20:24:30 +0100

    created event listener and function in .js file

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('button');
+const list = document.querySelector('.list');
+let counter = 1;
+
+const addListElement = function () {
+
+
+}
+
+btn.addEventListener('click', addListElement)
